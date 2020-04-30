960ffc256d09169bca683c0df4f8913b9e05ff98
JuanCarlosFrutos <juancarfruher@gmail.com>
2020-04-30 10:42:49 +0200

    Add transpiled version

--- index.js
+++
@@ -1,5 +0,0 @@
-import RenderMediaQuery from './src/RenderMediaQuery'
-import useMediaQuery from './src/useQueryListener'
-
-export { RenderMediaQuery }
-export default useMediaQuery
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import RenderMediaQuery from './RenderMediaQuery'
+import useMediaQuery from './useQueryListener'
+
+export { RenderMediaQuery }
+export default useMediaQuery
